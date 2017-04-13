4269e0856eacbe7f859ff7c71fce5180ed01f323
Sarthak Dev <sarthakdev90@gmail.com>
2017-04-13 12:17:15 +0530

    Removing default script

--- opsworks_deploy_python/recipes/default.rb
+++ opsworks_deploy_python/recipes/default.rb
@@ -2,17 +2,20 @@
 # Cookbook Name:: opsworks_deploy_python
 # Recipe:: default
 #
-node[:deploy].each do |application, deploy|
-  if deploy["custom_type"] != 'python'
-    next
-  end
-  python_base_setup do
-    deploy_data deploy
-    app_name application
-  end
+include_recipe "flask_test"
 
-  python_base_deploy do
-    deploy_data deploy
-    app_name application
-  end
-end
+Chef::Log.info "Default recipe"
+# node[:deploy].each do |application, deploy|
+#   if deploy["custom_type"] != 'python'
+#     next
+#   end
+#   python_base_setup do
+#     deploy_data deploy
+#     app_name application
+#   end
+
+#   python_base_deploy do
+#     deploy_data deploy
+#     app_name application
+#   end
+# end
